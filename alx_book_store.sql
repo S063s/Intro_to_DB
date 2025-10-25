35fc02656a8575345ca5204a585d6a30cb21306f
Shaun Manda <shaunwmanda@gmail.com>
2025-10-25 13:56:09 +0300

    edit author to capitalized the 'author' field in alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,27 +6,35 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
    publication_date DATE
 );
 
-CREATE TABLE authors (
+CREATE TABLE Books (
+    book_id serial primary key,
+    title VARCHAR(130),
+    author_id integer references Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors (
     author_id serial primary key,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id serial primary key,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id serial primary key,
-    customer_id integer references customers(customer_id),
+    customer_id integer references Customers(customer_id),
     order_date DATE
 );
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
    orderdetailid serial primary key,
-   order_id integer references orders(order_id),
-   book_id integer references books(book_id),
+   order_id integer references Orders(order_id),
+   book_id integer references Books(book_id),
    quantity double
 );
